ca84b30054cc2b30e69f3051f515bc1bb5eeedb5
Ubuntu <munawircm2mindt@munawircm2mindt.jcm4dlu4cczencb1n52dlttdfd.gx.internal.cloudapp.net>
2020-08-03 15:50:43 +0000

    first commit

---
+++ src/main/java/com/devops/Project1/Deockerizing_Jenkins_Pipeline/App.java
@@ -0,0 +1,13 @@
+package com.devops.Project1.Deockerizing_Jenkins_Pipeline;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ src/test/java/com/devops/Project1/Deockerizing_Jenkins_Pipeline/AppTest.java
@@ -0,0 +1,14 @@
+package com.devops.Project1.Deockerizing_Jenkins_Pipeline;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class AppTest {
+
+	@Test
+	public void test() {
+		fail("Not yet implemented");
+	}
+
+}
